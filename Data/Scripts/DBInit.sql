c0fe154ba88250a6955d92357745611039b58f16
BiReynolds <94270364+BiReynolds@users.noreply.github.com>
2026-06-01 21:55:17 +0000

    - Added sqlite3 dependency - Created first DBInit script

---
+++ Data/Scripts/DBInit.sql
@@ -0,0 +1,20 @@
+CREATE TABLE Projects (
+    Id INTEGER PRIMARY KEY,
+    ProjectName TEXT NOT NULL,
+    ParentId INTEGER,
+    TargetDate TEXT,
+    Priority INTEGER DEFAULT 0,
+    IsOngoing INTEGER DEFAULT 0,
+    IsComplete INTEGER DEFAULT 0
+);
+
+CREATE TABLE Tasks (
+    Id INTEGER PRIMARY KEY,
+    ProjectId INTEGER,
+    TaskName TEXT NOT NULL,
+    HourEstimate REAL,
+    IsComplete INTEGER DEFAULT 0,
+    CONSTRAINT fk_Project
+    FOREIGN KEY (ProjectId)
+    REFERENCES Projects(Id)
+)
